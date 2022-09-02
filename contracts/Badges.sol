c500b7920d13846957ae856615c387536454b7ff
Johnexzy <obajohn75@gmail.com>
2022-09-02 09:14:41 +0100

    :sparkles: (added nft badge contracts) An ERC1155 standard contract

---
+++ contracts/Badges.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.2;
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./IBadges.sol";
+contract DonorBadges is ERC1155, IBadges {
+    constructor()
+        ERC1155(
+            "https://api.afroapes.com/v1/collections/afro-apes-collectibles/{id}"
+        )
+    {}
+
+    function mintBadge(uint256 id, address to, bytes calldata category) external {
+        _mint(to, id, 1, category);
+    }
+
+
+}
---
+++ contracts/IBadges.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.2;
+
+interface IBadges {
+    function mintBadge(uint256 id, address _to, bytes calldata category) external;
+}
