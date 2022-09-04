570cdf1d83c8e98c11a334727fb25828f0f36433
Johnexzy <obajohn75@gmail.com>
2022-09-04 16:12:54 +0100

    :bookmark: (v1: added uniswap router for ETH->Token)

--- contracts/MaraScan.sol
+++ contracts/MaraScan.sol
@@ -4,11 +4,104 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./IBadges.sol";
+interface IUniswapV2Router02 {
+
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    function WETH() external pure returns (address);
+}
+interface ERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
 
 contract MaraScan is AccessControl, Initializable {
+    address internal constant UNISWAP_ROUTER_ADDRESS =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    IUniswapV2Router02 public uniswapRouter;
+
+    /** =====SUPPORTED ROLES======= **/
+    /** The Deployer is inherits the admin role */
+    bytes32 public constant ADMIN_ROLE = 0x00;
+
+    // ===Badge Contract ======
+    address public BADGE = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+
+    mapping(address => bool) public approvedTokens;
     event Disbursed(
         address indexed donor,
         uint256 amount,
@@ -21,20 +114,10 @@ contract MaraScan is AccessControl, Initializable {
         address indexed newContract,
         address indexed oldContract
     );
-    /** =====SUPPORTED TOKENS======= **/
-    // USDT address
-
-    /** =====SUPPORTED ROLES======= **/
-    /** The Deployer is inherits the admin role */
-    bytes32 public constant ADMIN_ROLE = 0x00;
-
-    // ===Badge Contract ======
-    address public BADGE = 0xd9145CCE52D386f254917e481eB44e9943F39138;
-
-    mapping(address => bool) public approvedTokens;
 
     function initialize() public initializer {
         _setupRole(ADMIN_ROLE, msg.sender);
+        uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
     }
 
     function donateAndDisburseToken(
@@ -54,7 +137,7 @@ contract MaraScan is AccessControl, Initializable {
         );
 
         // send tokens to contract
-        // it requires approval 
+        // it requires approval
         token.transferFrom(msg.sender, address(this), _amount);
 
         // Disburesement Logic Begins
@@ -71,8 +154,22 @@ contract MaraScan is AccessControl, Initializable {
         IBadges(BADGE).mintBadge(0, msg.sender, _category);
         emit Disbursed(msg.sender, _amount, _beneficiaries);
     }
-
-    function setAprovedToken(address _tokenAddress)
+    function SwapExactETHForTokens(
+        uint amountOut,
+        address token
+    ) external payable {
+        address[] memory path = new address[](2);
+        path[0] = uniswapRouter.WETH();
+        path[1] = token;
+        uniswapRouter.swapExactETHForTokens{ value: msg.value }(
+            amountOut,
+            path,
+            msg.sender,
+            block.timestamp + 50
+        );
+        
+    }
+    function ApproveTokenForDonation(address _tokenAddress)
         public
         onlyRole(ADMIN_ROLE)
     {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,45 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("@openzeppelin/hardhat-upgrades");
+// This is a sample Hardhat task. To learn how to create your own go to
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygon: {
+      url: process.env.POLYGON_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
-
-export default config;
